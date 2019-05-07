15efc028d3f491ee6f7589913f63b4318de843b0
DanLaZ <lazenberry_daniel@yahoo.com>
2019-05-07 18:40:41 -0400

    added all comments for the game logic

---
+++ logic.js
@@ -0,0 +1,32 @@
+/* Part 1: Building the Word Guess Game */
+
+/* Game Logic Part 1 - Utility Functions */
+
+// 1.0 - gameWords variable
+var gameWords = ("");
+
+//1.1 - randomWord function
+
+//1.2 - isCorrectGuess function
+
+//1.3 - getBlanks function
+
+//1.4 - fillBlanks function
+
+/* Game Logic Part 2 - Game Management Functions */
+
+//1.5 - setupRound function
+
+//1.6 - updateRound function
+
+//1.7 - hasWon function 
+
+//1.8 - hasLost function
+
+//1.9 - isEndOfRound function
+
+//1.10 - setupGame function
+
+//1.11 - startNewRound function
+
+//1.12 - myGame variable
